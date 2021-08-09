7e9eb7a1eaa63ebef25d88221fa4745acc634bf6
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2021-08-09 13:28:29 +0800

    disable linting of images

--- components/Tools.tsx
+++ components/Tools.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 
 const Tools = () => {
   return (
@@ -7,23 +6,13 @@ const Tools = () => {
       <ul className="flex space-x-5">
         <li className="flex space-x-2 items-center">
           <div className="bg-yellow-500 h-10 rounded-3xl w-10">
-            <Image
-              src="/img/water.png"
-              alt="Landscape picture"
-              width={50}
-              height={50}
-            />
+            <img src="/img/water.png" />
           </div>
           <span className="text-blue-500 font-bold">15/15</span>
         </li>
         <li className="flex space-x-2 items-center">
-          <div className="bg-yellow-500 h-10 rounded-3xl w-10">
-            <Image
-              src="/img/scarecrow.png"
-              alt="Landscape picture"
-              width={50}
-              height={50}
-            />
+        <div className="bg-yellow-500 h-10 rounded-3xl w-10">
+            <img src="/img/scarecrow.png" />
           </div>
           <span className="font-bold">5/5</span>
         </li>
