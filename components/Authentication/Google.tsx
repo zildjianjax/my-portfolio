efeaa304e2f5d48e1d53046427050c49ac0ff75d
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2021-08-09 21:23:06 +0800

    Fix google signup

--- components/Authentication/Google.tsx
+++ components/Authentication/Google.tsx
@@ -1,17 +1,27 @@
 import { auth, firestore, googleAuthProvider } from "../../lib/firebase";
-import React from 'react'
+import React from "react";
+import Alert from "sweetalert2";
 
 const Google = () => {
   const signInWithGoogle = async () => {
     const user = await auth.signInWithPopup(googleAuthProvider);
-    console.log(user);
-    
+    const whitelist: string[] | undefined[] | null[] = [
+      "zildjianjaxolis@gmail.com",
+      "dayanaracana@gmail.com",
+    ];
+    if (!whitelist.includes(user.user?.email as string)) {
+      auth.signOut();
+      Alert.fire("Oops!", "Unauthorized Access.", "error");
+      return false;
+    }
+
+    Alert.fire("Welcome to PVU Tracker", `Welcome, ${user.user?.displayName}`, "success");
   };
   return (
     <button className="btn btn-success-outline" onClick={signInWithGoogle}>
       Sign in with Google
     </button>
-  )
-}
+  );
+};
 
-export default Google
+export default Google;
