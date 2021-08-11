95c06b43d0724ba44685e81ce59fdcf4fef22edf
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2021-08-11 13:35:12 +0800

    update whitelist

--- lib/whitelist.ts
+++ lib/whitelist.ts
@@ -1,10 +1,10 @@
 export const admin: (string | undefined | null)[] = [
   "zildjianjaxolis@gmail.com",
   "dayanaracana@gmail.com",
+  "bontilaomelberth@gmail.com",
 ];
 export const guest: string[] | undefined[] | null[] = [
   "vernaizryjin@gmail.com",
-  "bontilaomelberth@gmail.com",
   "oliszildjianjax@gmail.com",
   "aldingumera@gmail.com",
   "lasijoe2gmail.com",
