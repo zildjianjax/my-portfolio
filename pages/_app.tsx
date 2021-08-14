f7d63ec1ff328452e1b64e47896146874720da01
Zildjian Jax Olis <zildjianjaxolis@gmail.com>
2021-08-14 09:14:35 +0800

    add analytics event

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,28 @@ import { UserContext } from "../lib/context";
 import { useUserData } from "../lib/hooks";
 import { useEffect } from "react"
 import { analytics } from "../lib/firebase";
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const routers = useRouter();
+
   useEffect(() => {
-    analytics();
+    if(process.env.NODE_ENV === 'production') {
+      const logEvent = (url: string) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent('screen_view');
+      }
+
+      routers.events.on('routeChangeComplete', logEvent);
+      //For First Page
+      logEvent(window.location.pathname);
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off('routeChangeComplete', logEvent);
+      };
+    }
+    
   }, [])
   const userData = useUserData();
   return (
